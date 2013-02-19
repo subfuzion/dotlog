bb8f9c7e81bf1d879b9ba0836886f8fd10a7a8d7
Tony Pujals <tony@subfuzion.com>
2013-02-19 07:46:54 -0800

    Updated shell script with comment not to use any more.

--- update-docs.sh
+++ update-docs.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-curl -X POST --data-urlencode content@README.md http://documentup.com/compiled > gendocs.html && open gendocs.html
+
+# DON'T USE THIS SCRIPT ANYMORE -- docs are updated automatically now
+
+# curl -X POST --data-urlencode content@README.md http://documentup.com/compiled > gendocs.html && open gendocs.html
