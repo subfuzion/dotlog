db5a9a41b6d5f3029b21b8e41f4d3d7ba1a7a613
Tony Pujals <tony@subfuzion.com>
2013-02-19 10:09:23 -0800

    Added DogLog.Sql reference to nuget package (temporary fix).

--- .deployment/DotLog/package.cmd
+++ .deployment/DotLog/package.cmd
@@ -1,4 +1,5 @@
 copy ..\..\dotlog\bin\Release\DotLog.dll lib\net40
-set VERSION=1.3.1
+copy ..\..\dotlog\bin\Release\DotLog.Sql.dll lib\net40
+set VERSION=1.3.2
 nuget pack DotLog.nuspec
 nuget push DotLog.%VERSION%.nupkg
