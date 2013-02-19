82418fb4754c675aeb97a51348e3e0978fde5b24
Tony Pujals <tony@subfuzion.com>
2013-02-19 07:16:59 -0800

    Added convenient extension methods for ILogger.

---
+++ dotlog/LogExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace DotLog
+{
+	public static class LogExtensions
+	{
+		public static LogEntry LogVerbose(this ILogger logger, string message, string category = null)
+		{
+			return logger.Log(message, LogLevel.Verbose, category);
+		}
+
+		public static LogEntry LogInformation(this ILogger logger, string message, string category = null)
+		{
+			return logger.Log(message, LogLevel.Information, category);
+		}
+
+		public static LogEntry LogWarning(this ILogger logger, string message, string category = null)
+		{
+			return logger.Log(message, LogLevel.Warning, category);
+		}
+
+		public static LogEntry LogError(this ILogger logger, string message, string category = null)
+		{
+			return logger.Log(message, LogLevel.Error, category);
+		}
+
+		public static LogEntry LogCritical(this ILogger logger, string message, string category = null)
+		{
+			return logger.Log(message, LogLevel.Critical, category);
+		}
+	}
+}
